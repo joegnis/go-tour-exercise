bcfc7093fdf05f2ea3562bc38d3d6fca8efe3faf
joegnis <joegnis@users.noreply.github.com>
2018-12-06 12:57:05 -0500

    Adds gitignore file and first exercise code

---
+++ exercise-slices.go
@@ -0,0 +1,18 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	ay := make([][]uint8, dy)
+	for j := range ay {
+		ay[j] = make([]uint8, dx)
+		for i := range ay[j] {
+			ay[j][i] = uint8(i^j)
+		}
+	}
+	return ay;
+}
+
+func main() {
+	pic.Show(Pic)
+}
