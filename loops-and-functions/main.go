bd2473f4c4e8c2260f8fed72090684957e7b86b9
joegnis <joegnis@users.noreply.github.com>
2018-12-07 16:24:43 -0500

    Adds loops-and-functions exercise

---
+++ loops-and-functions/main.go
@@ -0,0 +1,28 @@
+/*
+Computers typically compute the square root of x using a loop. 
+Starting with some guess z, we can adjust z based on how close z² is to x, producing a better guess:
+
+z -= (z*z - x) / (2*z)
+
+Repeating this adjustment makes the guess better and better 
+until we reach an answer that is as close to the actual square root as can be.
+*/
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z:=1.0
+	for oldZ:=0.0; math.Abs(z - oldZ) > 1e-15;  {
+		oldZ = z
+		z -= (z*z - x) / (2*z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(3))
+}
